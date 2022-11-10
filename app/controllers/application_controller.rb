f4c0ffdde4ea98c76f8b6ad09fed263298d43aab
Mostafa Sakr <msakr3@gmail.com>
2022-11-09 20:40:33 -0700

    Try to make heroku work.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
-require './lib/git_repo'
+# require './lib/git_repo'
 
 class ApplicationController < ActionController::Base
-  before_action :repo_info, :only => [:index, :show, :edit, :new]
+  # before_action :repo_info, :only => [:index, :show, :edit, :new]
 
-  def repo_info
-    repo_call = GitRepo.new
-    @repository_name = repo_call.repo_name
-    @group_usernames = repo_call.usernames
-    @contributor_commits = repo_call.commits_by_contributors
-    @pull_requests = repo_call.number_of_pull_requests
-  end
+  # def repo_info
+  #   repo_call = GitRepo.new
+  #   @repository_name = repo_call.repo_name
+  #   @group_usernames = repo_call.usernames
+  #   @contributor_commits = repo_call.commits_by_contributors
+  #   @pull_requests = repo_call.number_of_pull_requests
+  # end
 end
